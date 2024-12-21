0eb2d2b80c307274a02752e559e1a8851a4491c3
cuongkum <cuonghamanhcuong12@gmail.com>
2024-12-21 21:51:44 +0700

    update

--- src/components/zalo/components/header/name-header-chat-zalo-component.vue
+++ src/components/zalo/components/header/name-header-chat-zalo-component.vue
@@ -2,11 +2,11 @@
   <div class="absolute bottom-[2px] left-[50px]">
     <h3
       contenteditable="true"
-      class="text-[20px] !leading-4 font-medium text-white mb-1"
+      class="text-[18px] !leading-4 font-medium text-white mb-1"
     >
       Tên demo
     </h3>
-    <p contenteditable="true" class="text-base text-[#c6e1fb]">
+    <p contenteditable="true" class="text-sm text-[#c6e1fb]">
       Thời gian online
     </p>
   </div>
